493efa0c56e64a81010de463c59081c970828aeb
AdrienSldy <adriensldy@gmail.com>
2017-09-26 19:30:04 +0200

    Initialize Service Layer
    Add abstract logic to implement service objects.

---
+++ lib/service_layer.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'service_layer/version'
+
+# Service Layer pattern implementation.
+#
+# Provides an easy way to write services object. They are used to encapsulate
+# application logic business.
+module ServiceLayer
+end
