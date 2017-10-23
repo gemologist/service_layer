11e11de8a3b94b4ffcab72a94c29b15444f7e5de
Adrien Sasia -- Lefort des Ylouses <adriensldy@gmail.com>
2017-10-23 03:20:27 +0200

    Add explicit Response#failure?
    Signed-off-by: Adrien Sasia -- Lefort des Ylouses <adriensldy@gmail.com>

--- spec/service_layer/response/success_spec.rb
+++ spec/service_layer/response/success_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe ServiceLayer::Response::Success do
   describe '#success?' do
     it { is_expected.to be_success }
   end
+
+  describe '#failure?' do
+    it { is_expected.not_to be_failure }
+  end
 end
